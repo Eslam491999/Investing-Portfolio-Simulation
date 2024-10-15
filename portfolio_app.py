d859951fe849360ddc4cabf137c3a6907051c879
Eslam Ashraf <66190335+Eslam491999@users.noreply.github.com>
2024-10-15 22:06:15 +0300

    Update portfolio_app.py

--- portfolio_app.py
+++ portfolio_app.py
@@ -19,10 +19,6 @@ def calculate_weights(returns, strategy):
         weights = returns.std(axis=0)
         return weights / weights.sum()  # Normalize to sum to 1
 
-    elif strategy == "Market Neutral":
-        # Example market neutral: equal weight (can add more complex logic)
-        return np.ones(len(returns.columns)) / len(returns.columns)
-
     elif strategy == "Momentum":
         # Momentum: based on last period's returns
         weights = returns.iloc[-1]
@@ -95,7 +91,7 @@ def calculate_portfolio_performance(stocks, start_date, end_date, lookup_period,
 st.title('Investment Portfolio Creator')
 
 # User input: Select stocks
-available_stocks = ['AAPL', 'GOOGL', 'AMZN', 'MSFT', 'TSLA']
+available_stocks = ['AAPL', 'ADBE', 'AEYE', 'AMD', 'AMZN', 'ANET', 'ARKG', 'ARKK', 'ASML', 'AVGO', 'BILL', 'CELH', 'CMG', 'COIN', 'COST', 'CRM', 'CRWD', 'CYBR', 'DDOG', 'DKNG', 'DT', 'FTI', 'FTNT', 'GOOGL', 'GTEK', 'HUBS', 'INTC', 'KLAC', 'LCID', 'LLY', 'LPLA', 'LRCX', 'MA', 'MARA', 'MELI', 'META', 'MRVL', 'MSFT', 'MSI', 'MSTR', 'MU', 'NFLX', 'NIO', 'NOW', 'NVDA', 'ORCL', 'OXY', 'PANW', 'PATH', 'QCOM', 'RBLX', 'RIOT', 'RIVN', 'ROIV', 'ROKU', 'SMCI', 'SMH', 'SOUN', 'SP', 'SPCE', 'SQ', 'TEAM', 'TSLA', 'TSM', 'TWLO', 'U', 'UBER', 'V', 'VKTX', 'VRT', 'WDAY', 'XLE', 'XLF', 'ZM']
 selected_stocks = st.multiselect('Select stocks for your portfolio', available_stocks)
 
 # User input: Select date range
@@ -114,7 +110,7 @@ lookup_period = st.selectbox('Select lookup period', lookup_options)
 initial_cash = st.number_input('Initial Cash Amount', min_value=1000.0, value=100000.0)
 
 # User input: Select weight calculation strategy
-strategies = ['Equal Weight', 'Risk Parity', 'Risk Allocation', 'Market Neutral', 'Momentum']
+strategies = ['Equal Weight', 'Risk Parity', 'Risk Allocation', 'Momentum']
 selected_strategies = st.multiselect('Select weight calculation strategies', strategies)
 
 # Display button to start the simulation
